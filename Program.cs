6138db97726fd090b5ebb25899ec7909d9089ec0
natalya <natal1987natal@gmail.com>
2022-07-14 23:05:43 +0300

    добавила решение 2з

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numberA = new Random() .Next(-10, 11);
+Console.WriteLine(numberA);
+int numberB = new Random() .Next(-10, 11);
+Console.WriteLine(numberB);
+int max = 0;
+int min = 0;
+
+if (numberA > numberB)
+{
+    max = numberA;
+    min = numberB;
+    
+}
+else
+{
+    max = numberB;
+    min = numberA;
+}
+Console.WriteLine("max =" + max);
+Console.WriteLine("min =" + min);
